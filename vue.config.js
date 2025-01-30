30d1f640be9f460ea1fb3434a6e8b121078b2db9
LucasNovosado <165840126+LucasNovosado@users.noreply.github.com>
2025-01-30 20:20:53 -0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: 'Congregar'
+    }
+  },
   transpileDependencies: true,
   devServer: {
     headers: {
@@ -7,6 +13,59 @@ module.exports = defineConfig({
       'X-Content-Type-Options': 'nosniff',
       'X-Frame-Options': 'DENY',
       'X-XSS-Protection': '1; mode=block'
+    }
+  },
+  // Adiciona configuração PWA
+  pwa: {
+    name: 'Congregar',
+    themeColor: '#2c3e50',
+    msTileColor: '#2c3e50',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    manifestOptions: {
+      display: 'standalone',
+      background_color: '#ffffff',
+      icons: [
+        {
+          src: './img/icons/android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: './img/icons/android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        },
+        {
+          src: './img/icons/android-chrome-maskable-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+          purpose: 'maskable'
+        },
+        {
+          src: './img/icons/android-chrome-maskable-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable'
+        }
+      ]
+    },
+    workboxOptions: {
+      skipWaiting: true,
+      clientsClaim: true,
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp('^https://api\\.congregar\\.com/'),
+          handler: 'NetworkFirst',
+          options: {
+            networkTimeoutSeconds: 20,
+            cacheName: 'api-cache',
+            cacheableResponse: {
+              statuses: [0, 200]
+            }
+          }
+        }
+      ]
     }
   }
 })
